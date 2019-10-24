423c7d0f29493ba9018514c9fc6613e93647752c
LBDCode <42815395+LBDCode@users.noreply.github.com>
2019-10-24 14:04:04 -0400

    reworked nav and ToDo list view, added ajax call and partial view for table

---
+++ ToDoList/Custom_Scripts/BuildTable.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+
+    $.ajax({
+        url: '/ToDos/BuildToDoTable',
+        success: function (result) {
+            $('#tableDiv').html(result);
+        }
+    })
+})
